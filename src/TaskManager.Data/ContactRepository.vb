ab9f9cbbe3d653409468aa210b4fb501188fc78b
Junian Triajianto <author@junian.dev>
2025-09-03 14:56:35 +0700

    Add functions to contact CRUD

--- src/TaskManager.Data/ContactRepository.vb
+++ src/TaskManager.Data/ContactRepository.vb
@@ -4,17 +4,25 @@ Imports TaskManager.Core
 Public Class ContactRepository
     Implements IDisposable
 
+    Public Const ColumnId As String = "Id"
+    Public Const ColumnName As String = "Name"
+    Public Const ColumnEmail As String = "Email"
+    Public Const ColumnPhone As String = "Phone"
+    Public Const ColumnIsActive As String = "IsActive"
+    Public Const TableContact As String = "Contact"
+
     Public Sub Dispose() Implements IDisposable.Dispose
     End Sub
 
     Public Function InsertContact(contact As Contact) As Boolean
         Using connection As New SQLiteConnection(SQLiteHelper.DBPath)
             connection.Open()
-            Dim insertQuery As String = "INSERT INTO Contact (Name, Email, Phone) VALUES (@Email, @Name, @Phone)"
+            Dim insertQuery As String = $"INSERT INTO {TableContact} ({ColumnName}, {ColumnEmail}, {ColumnPhone}, {ColumnIsActive}) VALUES (@{ColumnName}, @{ColumnEmail}, @{ColumnPhone}, @{ColumnIsActive})"
             Using command As New SQLiteCommand(insertQuery, connection)
-                command.Parameters.AddWithValue("@Name", contact.Name)
-                command.Parameters.AddWithValue("@Email", contact.Email)
-                command.Parameters.AddWithValue("@Phone", contact.Phone)
+                command.Parameters.AddWithValue($"@{ColumnName}", contact.Name)
+                command.Parameters.AddWithValue($"@{ColumnEmail}", contact.Email)
+                command.Parameters.AddWithValue($"@{ColumnPhone}", contact.Phone)
+                command.Parameters.AddWithValue($"@{ColumnIsActive}", contact.IsActive)
                 command.ExecuteNonQuery()
             End Using
         End Using
@@ -22,16 +30,55 @@ Public Class ContactRepository
         Return False
     End Function
 
-    Public Function GetContacts() As List(Of Contact)
+    Public Function DeleteContact(contact As Contact) As Boolean
+        Using connection As New SQLiteConnection(SQLiteHelper.DBPath)
+            connection.Open()
+            Dim insertQuery As String = $"DELETE FROM {TableContact} WHERE {ColumnId}=@{ColumnId}"
+            Using command As New SQLiteCommand(insertQuery, connection)
+                command.Parameters.AddWithValue($"@{ColumnId}", contact.Id)
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+
+        Return False
+    End Function
+
+    Public Function UpdateContact(contact As Contact) As Boolean
+        Using connection As New SQLiteConnection(SQLiteHelper.DBPath)
+            connection.Open()
+            Dim insertQuery As String = $"UPDATE {TableContact} SET {ColumnName}=@{ColumnName}, {ColumnEmail}=@{ColumnEmail}, {ColumnPhone}=@{ColumnPhone}, {ColumnIsActive}=@{ColumnIsActive} WHERE {ColumnId}=@{ColumnId}"
+            Using command As New SQLiteCommand(insertQuery, connection)
+                command.Parameters.AddWithValue($"@{ColumnId}", contact.Id)
+                command.Parameters.AddWithValue($"@{ColumnName}", contact.Name)
+                command.Parameters.AddWithValue($"@{ColumnEmail}", contact.Email)
+                command.Parameters.AddWithValue($"@{ColumnPhone}", contact.Phone)
+                command.Parameters.AddWithValue($"@{ColumnIsActive}", contact.IsActive)
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+
+        Return False
+    End Function
+
+    Public Function GetContacts(Optional filterActive As Boolean = False) As List(Of Contact)
         Dim contacts As New List(Of Contact)
 
         Using connection As New SQLiteConnection(SQLiteHelper.DBPath)
             connection.Open()
-            Dim selectQuery As String = "SELECT Name, Email, 0 AS Phone FROM Contact WHERE 1 = 0"
+            Dim selectQuery As String = $"SELECT {ColumnId}, {ColumnName}, {ColumnEmail}, {ColumnPhone}, {ColumnIsActive} FROM {TableContact} "
+            If filterActive Then
+                selectQuery &= $" WHERE {ColumnIsActive} != 0"
+            End If
             Using command As New SQLiteCommand(selectQuery, connection)
                 Using reader As SQLiteDataReader = command.ExecuteReader()
                     While reader.Read()
-                        contacts.Add(New Contact() With {.Name = reader("Name"), .Email = reader("Email"), .Phone = reader("Phone")})
+                        contacts.Add(New Contact() With {
+                                     .Id = reader(ColumnId),
+                                     .Name = If(reader(ColumnName) Is DBNull.Value, "", reader(ColumnName)),
+                                     .Email = If(reader(ColumnEmail) Is DBNull.Value, "", reader(ColumnEmail)),
+                                     .Phone = If(reader(ColumnPhone) Is DBNull.Value, "", reader(ColumnPhone)),
+                                     .IsActive = reader(ColumnIsActive)
+                                     })
                     End While
                 End Using
             End Using
