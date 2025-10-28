c27e105e79f1b92db139038b1ef4661ec92ff579
Junian Triajianto <author@junian.dev>
2025-10-28 13:49:31 +0700

    Add basic winform project

---
+++ src/msgbox-to-xtramessagebox/MainForm.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        btnOkOnly = New Button()
+        SuspendLayout()
+        ' 
+        ' btnOkOnly
+        ' 
+        btnOkOnly.Location = New Point(46, 40)
+        btnOkOnly.Name = "btnOkOnly"
+        btnOkOnly.Size = New Size(75, 23)
+        btnOkOnly.TabIndex = 0
+        btnOkOnly.Text = "Ok"
+        btnOkOnly.UseVisualStyleBackColor = True
+        ' 
+        ' MainForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnOkOnly)
+        Name = "MainForm"
+        Text = "MainForm"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnOkOnly As Button
+End Class
---
+++ src/msgbox-to-xtramessagebox/Services/MsgBoxService.vb
@@ -0,0 +1,6 @@
+﻿Public Class MsgBoxService Implements IMessageBoxService
+    Public Function ShowMessage(message As String, title As String) As DialogResult Implements IMessageBoxService.ShowMessage
+        Return MsgBox(message, MsgBoxStyle.OkOnly, title)
+    End Function
+
+End Class
